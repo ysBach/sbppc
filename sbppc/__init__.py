8d9d97e63b4bd4d3712e3f379a8f40ec89195823
ysBach <ysbach93@gmail.com>
2026-01-23 12:16:46 +0900

    update init for easier use

--- sbppc/__init__.py
+++ sbppc/__init__.py
@@ -1,8 +1,2 @@
-from .functions import (
-    ppc_pars, ppc_p0s, ppc_bounds,
-    ppc_le, ppc_lm, ppc_sh3, ppc_sh5,
-    alpha_min_le,
-    log_likelihood_simple, log_prior_uniform, xy_minimum,
-    PPCModel
-)
-
+from .functions import *
+from .tool_emcee import *
